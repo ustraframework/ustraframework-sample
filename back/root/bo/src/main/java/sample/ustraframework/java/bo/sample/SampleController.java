51144eeed355e97080709f7f00628460cc900053
keymasroy <keymasroy@gmail.com>
2021-08-02 19:06:07 +0900

    gradle 빌드 오류 수정

--- back/root/bo/src/main/java/sample/ustraframework/java/bo/sample/SampleController.java
+++ back/root/bo/src/main/java/sample/ustraframework/java/bo/sample/SampleController.java
@@ -1,7 +1,6 @@
 package sample.ustraframework.java.bo.sample;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,12 +13,12 @@ public class SampleController {
 
 	@Autowired SampleService service;
 
-	@GetMapping("/api/test")
-	public SampleResVo test() {
-		SampleResVo vo = new SampleResVo();
-		vo.setTest("test");
-		return vo;
-	}
+//	@GetMapping("/api/test")
+//	public SampleResVo test() {
+//		SampleResVo vo = new SampleResVo();
+//		vo.setTest("test");
+//		return vo;
+//	}
 
 	@PostMapping("/api/sample/paging")
 	PaginationList<SampleModel> getCodeList(@RequestBody SampleModel.Criteria criteria) {
