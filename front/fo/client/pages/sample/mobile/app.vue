4bb12bfff29cd8a23f1e89c4a0a44032376e3df4
Jung, Chul Woo <wisehouse.kr@gmail.com>
2024-09-09 16:03:36 +0900

    에이전트 테스트

--- front/fo/client/pages/sample/mobile/app.vue
+++ front/fo/client/pages/sample/mobile/app.vue
@@ -7,7 +7,6 @@
         <div class="card-header-title">Status</div>
       </header>
       <div class="card-content">
-        UserAgent: {{ getUserAgent() }}
         native: {{ isNative }}<br />
         android: {{ isAndroid }}<br />
         iOS: {{ isIOs }}
@@ -99,10 +98,6 @@ export default class extends CustomFoComponent {
   isNative = false
   isAndroid = false
   isIOs = false
-
-  getUserAgent = () => {
-    return window.navigator.userAgent
-  }
 
   updateStatus = () => {
     this.isNative = this.$ustra.mobile.isNativeRequest
