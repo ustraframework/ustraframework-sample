ef44247ca3c35f565d4ebbdb0f08e4f8b1e2555d
jhjcoding <hjcoding@naver.com>
2025-06-20 14:52:58 +0900

    node 설정파일 수정

--- front/fo/layouts/default.vue
+++ front/fo/layouts/default.vue
@@ -8,5 +8,9 @@
 <script lang="ts">
 import LayoutNavbar from '@/components/layout/layout-navbar.vue'
 import LayoutFooter from '@/components/layout/layout-footer.vue'
+
+export default {
+  name: 'LayoutDefault',
+}
 </script>
 <style lang="scss"></style>
